6c1e1267d45c127265736160c9ebe46d42abd368
ShakedM1 <36158098+ShakedM1@users.noreply.github.com>
2018-02-12 20:47:10 +0200

    Add files via upload

---
+++ DNSSniffer-NEWVERSION/DNSSniffer/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace DNSSniffer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SelectInterface = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.InterfaceListBox = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SniffButton = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.ReportListView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // SelectInterface
+            // 
+            this.SelectInterface.Location = new System.Drawing.Point(267, 50);
+            this.SelectInterface.Name = "SelectInterface";
+            this.SelectInterface.Size = new System.Drawing.Size(113, 42);
+            this.SelectInterface.TabIndex = 0;
+            this.SelectInterface.Text = "Select Network Interface";
+            this.SelectInterface.UseVisualStyleBackColor = true;
+            this.SelectInterface.Click += new System.EventHandler(this.SelectInterface_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Step one: Select desired network interface";
+            // 
+            // InterfaceListBox
+            // 
+            this.InterfaceListBox.FormattingEnabled = true;
+            this.InterfaceListBox.Location = new System.Drawing.Point(399, 50);
+            this.InterfaceListBox.Name = "InterfaceListBox";
+            this.InterfaceListBox.Size = new System.Drawing.Size(234, 82);
+            this.InterfaceListBox.TabIndex = 2;
+            this.InterfaceListBox.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 202);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Step two: Start sniffing";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // SniffButton
+            // 
+            this.SniffButton.Location = new System.Drawing.Point(267, 189);
+            this.SniffButton.Name = "SniffButton";
+            this.SniffButton.Size = new System.Drawing.Size(113, 39);
+            this.SniffButton.TabIndex = 4;
+            this.SniffButton.Text = "Sniff";
+            this.SniffButton.UseVisualStyleBackColor = true;
+            this.SniffButton.Click += new System.EventHandler(this.SniffButton_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // ReportListView
+            // 
+            this.ReportListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.ReportListView.Location = new System.Drawing.Point(32, 246);
+            this.ReportListView.Name = "ReportListView";
+            this.ReportListView.Size = new System.Drawing.Size(577, 226);
+            this.ReportListView.TabIndex = 5;
+            this.ReportListView.UseCompatibleStateImageBehavior = false;
+            this.ReportListView.View = System.Windows.Forms.View.Details;
+            this.ReportListView.SelectedIndexChanged += new System.EventHandler(this.ReportListView_SelectedIndexChanged_1);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Time:";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "IP:";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Report:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 501);
+            this.Controls.Add(this.ReportListView);
+            this.Controls.Add(this.SniffButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.InterfaceListBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SelectInterface);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SelectInterface;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox InterfaceListBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button SniffButton;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ListView ReportListView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+    }
+}
+
---
+++ DNSSniffer-NEWVERSION/DNSSniffer/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PcapDotNet.Core;
+using System.Windows.Forms;
+using System.Windows;
+using PcapDotNet.Packets;
+using PcapDotNet.Packets.Dns;
+using PcapDotNet.Packets.IpV4;
+using PcapDotNet.Packets.Transport;
+
+namespace DNSSniffer
+{
+
+    public partial class Form1 : Form
+    {
+        public int CaptureIndex;
+        //get all live capture devices
+        IList<LivePacketDevice> allDevices = LivePacketDevice.AllLocalMachine;
+        public Form1()
+        {
+            InitializeComponent();
+            GetCaptureDevices();
+        }
+
+
+        private void SelectInterface_Click(object sender, EventArgs e)
+        {            
+            int selected;
+            
+            if (InterfaceListBox.SelectedItem != null)
+            {
+                selected = InterfaceListBox.SelectedIndex;
+                MessageBox.Show(selected.ToString());
+                CaptureIndex = selected;
+            }
+            else
+                MessageBox.Show("Please select an interface");
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }//
+
+        public void GetCaptureDevices()
+        {
+            //check if there are any available
+            if (allDevices.Count == 0)
+            {
+                MessageBox.Show("No interfaces found! Make sure WinPcap is installed.");
+                return;
+            }
+
+            // append all the network interfaces to the listbox
+            for (int i = 0; i != allDevices.Count; ++i)
+            {
+                LivePacketDevice device = allDevices[i];
+                string str = "";
+                if (device.Description != null)
+                    str += ((i + 1) + ". " + device.Description);
+                else
+                    str += ((i + 1) + ". " + " (No description available)");
+
+                InterfaceListBox.Items.Add(str);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SniffButton_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.IsBusy != true)
+            {
+                // Start the asynchronous operation.
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        //To be activated only on DNS packets!(port 53 tcp or udp
+        public static DnsDomainName[] ExtractDnsQueries(Packet pkt)
+        {
+            IpV4Datagram ip = pkt.Ethernet.IpV4;
+            UdpDatagram udp = ip.Udp;
+            DnsDatagram dns = udp.Dns;
+            //check if dns datagram is query
+            if (dns.IsQuery)
+            {
+                //No check for query count is needed becuase the number of queries is equal to querycount
+                DnsDomainName[] names = new DnsDomainName[dns.QueryCount];
+                int i = 0;
+                foreach (DnsQueryResourceRecord record in dns.Queries)
+                {
+                    names[i] = record.DomainName;
+                    i++;
+                }
+                return names;
+            }
+            return null;
+            //returns null if dns is not query
+
+        }//extreactqueries
+
+        public void AddToListView(string time, string ip, string report)
+        {
+            ListViewItem item = new ListViewItem();
+            item.Text = time;        
+            //item.SubItems.Add(time);
+            item.SubItems.Add(ip);
+            item.SubItems.Add(report);
+            ReportListView.Items.Add(item);
+            //ReportListView.Items[ReportListView.Items.Count - 1].EnsureVisible();
+        }
+
+        private void ReportListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // Take the selected adapter
+            PacketDevice selectedDevice = allDevices[CaptureIndex];
+
+            // Open the device
+            using (PacketCommunicator communicator =
+                selectedDevice.Open(65536,                                  // portion of the packet to capture
+                                                                            // 65536 guarantees that the whole packet will be captured on all the link layers
+                                    PacketDeviceOpenAttributes.Promiscuous, // promiscuous mode
+                                    1000))                                  // read timeout
+            {
+                MessageBox.Show("Listening on " + selectedDevice.Description + "...");
+
+                // Retrieve the packets
+                Packet packet;
+                do
+                {
+                    PacketCommunicatorReceiveResult result = communicator.ReceivePacket(out packet);
+                    using (BerkeleyPacketFilter filter = communicator.CreateFilter("port 53"))
+                    {
+                        // Set the filter
+                        communicator.SetFilter(filter);
+                    }
+                    switch (result)
+                    {
+                        case PacketCommunicatorReceiveResult.Timeout:
+                            // Timeout elapsed
+                            continue;
+                        case PacketCommunicatorReceiveResult.Ok:
+
+                            string timestamp = packet.Timestamp.ToString("yyyy-MM-dd hh:mm:ss.fff");
+
+                            //Console.WriteLine(packet.Timestamp.ToString("yyyy-MM-dd hh:mm:ss.fff") + " length:" +
+                            //                  packet.Length);
+                            DnsDomainName[] names = ExtractDnsQueries(packet);
+                            IpV4Datagram ip = packet.Ethernet.IpV4;
+                            UdpDatagram udp = ip.Udp;
+                            DnsDatagram dns = udp.Dns;
+
+
+                            if (names != null)
+                            {
+
+                                foreach (DnsDomainName domain in names)
+                                {
+
+                                    //if (domain == null)
+                                    //    Console.WriteLine("No queries");
+
+                                    if (domain != null)
+                                    {
+                                        AddToListView(timestamp, ip.Source.ToString(), domain.ToString());
+                                        //Console.WriteLine(domain.ToString());
+                                    }
+                                }
+                            }
+
+                            break;
+                        default:
+                            throw new InvalidOperationException("The result " + result + " should never be reached here");
+                    }
+                } while (true);
+            }
+        
+        }//background worker
+
+        private void ReportListView_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
